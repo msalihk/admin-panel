801f9a9496488004cc1c010e79aa9cc5157342f2
Salih Küçükkayış <skucukkayis@gmail.com>
2023-08-26 10:23:37 +0300

    Clearing the routes

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Sorting;
+
+
+class HomeController extends Controller
+{
+    public function index() : View
+    {
+        $categories = Category::where('is_active', 1)->get();
+        $footerCategories = Category::where('is_active',1)->where('is_shown_in_footer', 1)->get();
+        $sortedPosts = Sorting::where('location', 1)->orderBy('order', 'asc')->get();
+        $headlineRightNews = Sorting::where('location', 2)->orderBy('order', 'asc')->get();
+        $editorsPicks = Post::where('is_active', 1)->orderBy('created_at', 'desc')->get();
+        $editorsPicksBottom = Post::where('is_active', 1)->orderBy('created_at', 'desc')->skip(1)->take(6)->get();
+        $newsPosts = Post::where('location', 0)->whereHas('categories', function ($query) {
+                $query->where('name', "News");
+            })
+            ->orderBy('created_at', 'desc')
+            ->take(3)->get();
+        $sportsPosts = Post::where('location', 0)->whereHas('categories', function ($query) {
+                $query->where('name', "Sports");
+            })
+            ->orderBy('created_at', 'desc')
+            ->take(3)->get();
+        $topStoryIndex = 1;
+        $worldInPictures = Post::where('is_active', 1)->orderBy('created_at', 'desc')->take(5)->get();
+
+        return view('pages.home', compact('categories', 'footerCategories', 'sortedPosts', 'headlineRightNews',
+                    'editorsPicks','editorsPicksBottom', 'newsPosts', 'sportsPosts', 'topStoryIndex', 'worldInPictures'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SpecialSortController;
 use App\Models\Category;
@@ -21,30 +22,9 @@ use function Laravel\Prompts\search;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home', [
-        'categories' => Category::where('is_active', 1)->get(),
-        'footerCategories' => Category::where('is_active',1)->where('is_shown_in_footer', 1)->get(),
-        'sortedPosts' => Sorting::where('location', 1)->orderBy('order', 'asc')->get(),
-        'headlineRightNews' => Sorting::where('location', 2)->orderBy('order', 'asc')->get(),
-        'editorsPicks' => Post::where('is_active', 1)->orderBy('created_at', 'desc')->get(),
-        'editorsPicksBottom' => Post::where('is_active', 1)->orderBy('created_at', 'desc')->skip(1)->take(6)->get(),
-        'newsPosts' => Post::where('location', 0)->whereHas('categories', function ($query) {
-            $query->where('name', "News");
-        })->orderBy('created_at', 'desc')
-        ->take(3)->get(),
-        'sportsPosts' => Post::where('location', 0)->whereHas('categories', function ($query) {
-            $query->where('name', "Sports");
-        })->orderBy('created_at', 'desc')
-        ->take(3)->get(),
-        'topStoryIndex' => 1,
-        'worldInPictures' => Post::where('is_active', 1)->orderBy('created_at', 'desc')->take(5)->get()
-    ]);
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/categories/news', [PostController::class, 'navigation'])->name('news');
-
-
 
 Route::get('/search', [PostController::class, 'search'])->name('search');
 
